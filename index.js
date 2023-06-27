d688af9f463561ebcc0f2aa1a76f2b6559100482
BongSuChoi <chlqhdtn98@gmail.com>
2023-06-27 16:10:13 +0900

    modify example

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import { resolve } from 'path';
 	const imageDirPath = resolve('./images/target');
 	const imageDistPath = resolve('./images/dist');
 	const files = fs.readdirSync(imageDirPath);
-	const fileLength = files.length;
-	files.forEach((file, idx) => {
+	files.forEach((file, index) => {
 		// console.log('file : ', file);
 
 		/**
@@ -15,18 +14,18 @@ import { resolve } from 'path';
 
 		// ---------- EXAMPLE 01 - simple replace ---------- //
 		// const name = file.replace('-a', '-b');
-		// fs.copyFile(imageDirPath + `/${file}`, imageDistPath + `/${name}`, (err) => {
+		// fs.copyFile(`imageDirPath/${file}`, `imageDistPath/${name}`, (err) => {
 		// 	if (err) throw err;
-		// 	console.log(file, '=> ' + name);
+		// 	console.log(`${file} => ${name}`);
 		// });
 
 		// ---------- EXAMPLE 02 - add ascending ---------- //
-		// const lengthDigit = fileLength.toString().length;
-		// const count = idx.toString().padStart(lengthDigit, '0');
+		// const lengthDigit = files.length.toString().length;
+		// const count = index.toString().padStart(lengthDigit, '0');
 		// const name = file.replace('-a', `-a-${count}`);
-		// fs.copyFile(imageDirPath + `/${file}`, imageDistPath + `/${name}`, (err) => {
+		// fs.copyFile(`imageDirPath/${file}`, `imageDistPath/${name}`, (err) => {
 		// 	if (err) throw err;
-		// 	console.log(file, '=> ' + name);
+		// 	console.log(`${file} => ${name}`);
 		// });
 	});
 })();
